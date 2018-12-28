ddaaf118e0b3700117b76e9b2ec7c3bda0c539b6
lh <642404044@qq.com>
2018-12-28 14:26:10 +0800

    hello websocket

--- backstage/imooc-netty-hello/src/main/java/com/imooc/netty/websocket/WSServer.java
+++ backstage/imooc-netty-hello/src/main/java/com/imooc/netty/websocket/WSServer.java
@@ -9,18 +9,14 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class WSServer {
 
 	public static void main(String[] args) throws Exception {
-		
 		EventLoopGroup mainGroup = new NioEventLoopGroup();
 		EventLoopGroup subGroup = new NioEventLoopGroup();
-		
 		try {
 			ServerBootstrap server = new ServerBootstrap();
 			server.group(mainGroup, subGroup)
 				.channel(NioServerSocketChannel.class)
 				.childHandler(new WSServerInitialzer());
-			
 			ChannelFuture future = server.bind(8088).sync();
-			
 			future.channel().closeFuture().sync();
 		} finally {
 			mainGroup.shutdownGracefully();
